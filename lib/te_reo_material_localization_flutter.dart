2c47db5259ab73e2bcc02fc704d32956e91f9787
Dunatron <heath.dunlop.hd@gmail.com>
2021-11-24 22:26:48 +1300

    ff

---
+++ lib/te_reo_material_localization_flutter.dart
@@ -0,0 +1,7 @@
+library te_reo_material_localization_flutter;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ test/te_reo_material_localization_flutter_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:te_reo_material_localization_flutter/te_reo_material_localization_flutter.dart';
+
+void main() {
+  test('Honestly cant think of a test to run...', () {});
+}
